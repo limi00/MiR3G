d4db14d3513d470b507da7f7792857790111e86a
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-11 15:16:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
 sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
-# sed -i '$a src-git luci1 https://github.com/coolsnowwolf/luci.git' feeds.conf.default
+sed -i '$a src-git luci1 https://github.com/coolsnowwolf/luci.git' feeds.conf.default
 sed -i '$a src-git lean https://github.com/coolsnowwolf/packages' feeds.conf.default
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
